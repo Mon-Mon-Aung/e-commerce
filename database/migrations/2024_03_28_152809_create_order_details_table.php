84b39023e1bfd537da1716670cae7b280520418c
Mon-Mon-Aung <157705325+Mon-Mon-Aung@users.noreply.github.com>
2024-03-28 22:55:58 +0630

    add tables

--- database/migrations/2024_03_28_152809_create_order_details_table.php
+++ database/migrations/2024_03_28_152809_create_order_details_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('city');
+            $table->string('state', 45)->nullable();
+            $table->string('zipcode', 45);
+            $table->string('country_code', 3);
             $table->timestamps();
         });
     }
